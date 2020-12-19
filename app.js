dfe0699ba103a55cc746a759adb8ff3c90e2bf17
SOKu-ma <sokumaeng2020@gmail.com>
2020-12-19 21:04:34 +0900

    mod function

--- app.js
+++ app.js
@@ -8,17 +8,18 @@ const csvFilePath = './data/csvData.csv';
 
 const port = process.env.port || 8080;
 
-app.get('/api', async (req, res, next) => {
+app.get('/api', async function (req, res, next) {
         try {
             const jsonArray = await csv().fromFile(csvFilePath);
             res.json(jsonArray);
+            next();
         }
         catch {
             next(error);
         }
     })
 
-app.use((err, req, res) => {
+app.use(function(err, req, res, next) {
         console.error(err);
         res.status(500).send('Internal Server Error');
     });
