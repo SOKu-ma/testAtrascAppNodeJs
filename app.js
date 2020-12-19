ed9f762134b098546eff7bc3357815dfa36c3f0f
SOKu-ma <sokumaeng2020@gmail.com>
2020-12-20 00:31:23 +0900

    mod func

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var app = express();
 var csv = require('csvtojson');
+var fs = require('fs');
 
 const csvFilePath = './data/csvData.csv';
 const port = process.env.port || 8080;
 
 app.get('/', async function (req, res, next) {
-        // try {
-        //     const jsonArray = await csv().fromFile(csvFilePath);
-        //     res.json(jsonArray);
-        //     // next();
-        // }
-        // catch {
-        //     next(error);
-        // }
-        res.send('Hello Node.js');
+        try {
+            let text = fs.readFileSync(csvFilePath, 'utf-8');
+            // console.log('readFile fin:' + text);
+            const jsonArray = await csv().fromFile(csvFilePath);
+            res.json(jsonArray);
+            // next();
+        }
+        catch {
+            next(console.err);
+        }
     })
 
 app.use(function(err, req, res) {
