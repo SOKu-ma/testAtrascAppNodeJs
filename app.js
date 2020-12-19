1186d29ee08cdf57339ab9f51bd2b31b5f4a1ded
SOKu-ma <sokumaeng2020@gmail.com>
2020-12-19 22:47:41 +0900

    mod func

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 var express = require('express');
 var app = express();
-
-// var fs = require("fs");
 var csv = require('csvtojson');
 
 const csvFilePath = './data/csvData.csv';
-
 const port = process.env.port || 8080;
 
-app.get('/api', async function (req, res, next) {
+app.get('/', async function (req, res, next) {
         try {
             const jsonArray = await csv().fromFile(csvFilePath);
             res.json(jsonArray);
-            next();
+            // next();
         }
         catch {
             next(error);
         }
     })
 
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
         console.error(err);
         res.status(500).send('Internal Server Error');
-        next();
     });
 
 var server = app.listen(port, () => {
