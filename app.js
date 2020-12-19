5ba7bf30059c75b5042be37b49ea714a5da7d573
SOKu-ma <sokumaeng2020@gmail.com>
2020-12-19 23:25:39 +0900

    test

--- app.js
+++ app.js
@@ -6,21 +6,22 @@ const csvFilePath = './data/csvData.csv';
 const port = process.env.port || 8080;
 
 app.get('/', async function (req, res, next) {
-        try {
-            const jsonArray = await csv().fromFile(csvFilePath);
-            res.json(jsonArray);
-            // next();
-        }
-        catch {
-            next(error);
-        }
+        // try {
+        //     const jsonArray = await csv().fromFile(csvFilePath);
+        //     res.json(jsonArray);
+        //     // next();
+        // }
+        // catch {
+        //     next(error);
+        // }
+        res.send('Hello Node.js');
     })
 
-// app.use(function(err, req, res) {
-//         console.error(err);
-//         console.log(res.status);
-//         res.status(500).send('Internal Server Error');
-//     });
+app.use(function(err, req, res) {
+        console.error(err);
+        console.log(res.status);
+        res.status(500).send('Internal Server Error');
+    });
 
 var server = app.listen(port, () => {
     console.log('Node.js is listeninig to PORT:' + server.address().port);
